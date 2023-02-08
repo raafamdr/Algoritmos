6e55318451682ca40ea5f23cbb43022b5e48145d
raafamdr <rafael.delrio02@gmail.com>
2023-02-08 01:48:13 -0300

    Adicionando selection_sort.py

---
+++ Ordenação/selection_sort.py
@@ -0,0 +1,16 @@
+def busca_menor(arr):
+    menor = arr[0]
+    menor_indice = 0
+    for i in range(1, len(arr)):
+        if arr[i] < menor:
+            menor = arr[i]
+            menor_indice = i
+    return menor_indice
+
+
+def ordenacao_por_selecao(arr):
+    novo_arr = []
+    for i in range(len(arr)):
+        menor = busca_menor(arr)
+        novo_arr.append(arr.pop(menor))
+    return novo_arr
